5d9ab85d16afece2d272d3a7799ee2e40b81865c
yifei20 <yifei.du20@outlook.com>
2023-06-01 02:18:50 +0800

    使用leetcode editor插件重构练习仓库

---
+++ src/leetcode/editor/cn/[26] 删除有序数组中的重复项.java
@@ -0,0 +1,28 @@
+package leetcode.editor.cn;
+
+class RemoveDuplicatesFromSortedArray{
+    public static void main(String[] args) {
+        Solution solution = new RemoveDuplicatesFromSortedArray().new Solution();
+        
+    }
+    
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n = nums.length;
+        if (n == 0)
+            return 0;
+        int i = 1, j = 1;
+        while (j < n) {
+            if (nums[j] != nums[j-1]) {
+                nums[i] = nums[j];
+                i++;
+            }
+            j++;
+        }
+        return i;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
